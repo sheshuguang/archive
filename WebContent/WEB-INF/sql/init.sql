766f7302e8c5a229c75d0e9ae4512aa89f18be91
sheshuguang <sheshuguang@gmail.com>
2012-08-05 16:57:25 +0800

    init.sql

--- WebContent/WEB-INF/sql/init.sql
+++ WebContent/WEB-INF/sql/init.sql
@@ -1,4 +1,4 @@
--------------archivedb 初始化数据 --------------
+﻿-------------archivedb 初始化数据 --------------
 -------------适用于Hsqldb 2.0
 
 --sys_config 系统配置表
@@ -16,10 +16,10 @@ INSERT INTO SYS_ORG (ORGID,ORGNAME,PARENTID,ORGORDER) VALUES ('2','默认组',1,
 INSERT INTO SYS_role (ROLEID,ROLENAME,ROLEMEMO) VALUES ('1','一般用户','系统初始化基础角色');
 
 --sys_function 功能菜单表
-insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('1','系统维护','SYSTEMP','',-1,1,0,,'icon-sysmanage');
+insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('1','系统维护','SYSTEMP','',-1,1,0,'icon-sysmanage');
 insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('2','系统配置','CONFIG','webpage/system/config/ConfigMgr.html',1,1,1,'');
-insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('3','帐户管理','ACCOUNT','webpage/system/account/AccountMgr.html',2,1,1.'icon-user');
-insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('4','档案库维护','TEMPLET','webpage/archive/templet/TempletMgr.html',3,1,1.'icon-table');
+insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('3','帐户管理','ACCOUNT','webpage/system/account/AccountMgr.html',2,1,1,'icon-user');
+insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('4','档案库维护','TEMPLET','webpage/archive/templet/TempletMgr.html',3,1,1,'icon-table');
 insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('5','档案管理','ARCHIVEMANAGE','',-1,1,0,'icon-page-gear');
 insert into sys_function(FUNCTIONID,FUNCHINESENAME,FUNENGLISHNAME,FUNPATH,FUNORDER,FUNSYSTEM,FUNPARENT,FUNICON) values ('6','档案管理','ARCHIVE','webpage/archive/archive/ArchiveMgr.html',1,1,5,'icon-page');
 
